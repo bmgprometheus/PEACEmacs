932be2f17e8a997db224dbda622f4e9e326bccec
Ben Gabriel <bmgabriel@protonmail.com>
2024-08-04 14:49:16 -0700

    add c and c++ hooks to PEACE/cedet-hook

--- custom/init-custom-dev-c-lang.el
+++ custom/init-custom-dev-c-lang.el
@@ -39,7 +39,9 @@
   (local-set-key "\C-c\C-j" 'semantic-ia-fast-jump)
   (local-set-key "\C-c\C-s" 'semantic-ia-show-summary))
 
-
+(add-hook 'c-mode-common-hook 'PEACE/cedet-hook)
+(add-hook 'c-mode-hook 'PEACE/cedet-hook)
+(add-hook 'c++-mode-hook 'PEACE/cedet-hook)
 
 (provide 'init-custom-dev-c-lang)
 ;;; init-custom-dev-c-lang.el ends here
