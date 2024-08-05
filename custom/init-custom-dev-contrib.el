4938e37134f93b834d058686757c524dae113915
Ben Gabriel <bmgabriel@protonmail.com>
2024-08-04 18:38:42 -0700

    add global key C-a binding to defun prelude-move-beginning-of-line

---
+++ custom/init-custom-dev-contrib.el
@@ -0,0 +1,57 @@
+;;; init-custom-dev-contrib.el -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2024  B.M. Gabriel <bmgabriel@protonmail.com>
+
+;; Author: B.M. Gabriel <bmgabriel@protonmail.com>
+;; Keywords: 
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; 
+
+;;; Code:
+
+;; custom functions
+(defun prelude-move-beginning-of-line (arg)
+  "Move point back to indentation of beginning of line.
+
+Move point to the first non-white-space character on this line.
+If point is already there, move to the beginning of the line.
+Effectively toggle between the first non-whitespace character and
+the beginning of the line.
+
+If arg is not nil or 1 move forward arg-1 lines first. If point
+reaches the beginning or end of the buffer, stop there."
+
+  (interactive "^p")
+  (setq arg (or arg 1))
+
+  ;; Move lines first
+  (when (/= arg 1)
+    (let ((line-move-visual nil))
+      (forward-line (1- arg))))
+
+  (let ((orig-point (point)))
+    (back-to-indentation)
+    (when (= orig-point (point))
+      (move-beginning-of-line 1))))
+
+(global-set-key (kbd "C-a") 'prelude-move-beginning-of-line)
+
+
+(provide 'init-custom-dev-contrib)
+;;; init-custom-dev-contrib.el ends here
+  
