dc272150f5236f8e987b60f0fca36dd5cc02ebca
Ben Gabriel <bmgabriel@protonmail.com>
2024-08-04 18:47:19 -0700

    modify kill-region to kill a single line when no active region

--- custom/init-custom-dev-contrib.el
+++ custom/init-custom-dev-contrib.el
@@ -60,6 +60,13 @@ reaches the beginning or end of the buffer, stop there."
      (list (line-beginning-position)
 	   (line-beginning-position 2)))))
 
+(defadvice kill-region (before slick-cut activate compile)
+  "When called interactively with no active region, kill a single
+ line instead."
+  (interactive
+   (if mark-active (list (region-beginning) (region-end))
+     (list (line-beginning-position)
+	   (line-beginning-position 2)))))
 
 (provide 'init-custom-dev-contrib)
 ;;; init-custom-dev-contrib.el ends here
