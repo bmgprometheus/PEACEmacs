33a7725566ee30ab73bfa69a172a6bba820cd9bb
Ben Gabriel <bmgabriel@protonmail.com>
2024-08-05 14:26:30 -0700

    add unicode support for clipbord selections

--- custom/init-custom-languages.el
+++ custom/init-custom-languages.el
@@ -33,6 +33,9 @@
 (setq buffer-file-coding-system 'utf-8)
 (prefer-coding-system 'utf-8)
 
+(unless (eq system-type 'windows-nt)
+  (set-selection-coding-system 'utf-8))
+
 
 
 (provide 'init-custom-languages)
