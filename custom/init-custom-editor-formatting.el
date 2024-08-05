3a935ef312cad50519ea8d35e9880713a2f74c5c
Ben Gabriel <bmgabriel@protonmail.com>
2024-08-05 13:44:59 -0700

    add editor indentation modes

---
+++ custom/init-custom-editor-formatting.el
@@ -0,0 +1,48 @@
+;;; init-custom-editor-formatting.el -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2024  B.M. Gabriel <bmgabriel@protonmail.com>
+
+;; Author: B.M. Gabriel <bmgabriel@protonmail.com>
+;; Keywords: 
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; 
+
+;;; Code:
+
+;; PACKAGE: EMACS
+;; GROUP: Editor -> Indentation
+(setq-default indent-tabs-mode nil)	; space indentation
+(setq-default tab-width 4)
+(setq sentence-end-double-space nil)
+(delete-selection-mode)
+(global-set-key (kbd "RET") 'newline-and-indent)
+
+(add-hook 'sh-mode-hook (lambda ()
+			  (setq tab-width 4)))
+(add-hook 'prog-mode-hook		; show unnecessary whitespace
+	  (lambda() (interacive)
+	    (setq show-trailing-whitespace 1)))
+
+(global-set-key (kbd "C-c w")		; mode for whitespace
+		'whitespace-mode)
+
+
+
+
+(provide 'init-custom-editor-formatting)
+;;; init-custom-editor-formatting.el ends here
