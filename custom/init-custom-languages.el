3b8c4921c4240996a733ebdd8f9d8b7226afc31a
Ben Gabriel <bmgabriel@protonmail.com>
2024-08-06 15:13:23 -0700

    fix typo utf-8 local-coding-system

--- custom/init-custom-languages.el
+++ custom/init-custom-languages.el
@@ -29,7 +29,7 @@
 (set-language-environment 'utf-8)	; unicode support UTF-8
 (set-default-coding-systems 'utf-8)
 (set-terminal-coding-system 'utf-8)
-(setq locale-coding-system 'utif-8)
+(setq locale-coding-system 'utf-8)
 (setq buffer-file-coding-system 'utf-8)
 (prefer-coding-system 'utf-8)
 
