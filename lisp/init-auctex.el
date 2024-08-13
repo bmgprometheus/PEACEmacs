183fcc11d4dcd999ef197c3ea40da99bc658be40
Ben Gabriel <bmgabriel@protonmail.com>
2024-08-13 15:09:17 -0700

    add use-package config for auctex

--- lisp/init-auctex.el
+++ lisp/init-auctex.el
@@ -29,7 +29,80 @@
 ;;              from inside Emacs.
 
 ;;; Code:
+(use-package auctex
+  :straight (:type git :host nil :repo "https://git.savannah.gnu.org/git/auctex.git"
+                   :pre-build (
+                               ("./autogen.sh")
+                               ("./configure" "--without-textmf-dir" "with-lispdir=.")
+                               ("make")
+                               ;; or (shell-command "./autogen.sh &&
+                               ;; ./configure --without-texmf-dir
+                               ;; --with-lispdir=. && make")
+                               )
+                   )
+  :mode
+  ;; https://www.mail-archive.com/auctex@gnu.org/msg07608.html
+  ;; https://www.gnu.org/software/emacs/manual/html_node/reftex/Installation.html
+  ("\\.tex\\'" . latex-mode)            ; Activate inferior Emacs latex mode
+  :hook
+        (LaTeX-mode . TeX-PDF-mode)
+        (LaTeX-mode . company-mode)
+        (LaTeX-mode . flyspell-mode)
+        (LaTeX-mode . flycheck-mode)
+        (LaTeX-mode . LaTeX-math-mode)
+        (LaTeX-mode . turn-on-reftex)
+        (LaTeX-mode . turn-on-cdlatex)
+  :init
+  (load "auctex.el" nil t t)
+  (load "preview-latex.el" nil t t)
+  (require 'reftex)
 
+  (setq-default TeX-master 'dwim)
+
+  (setq TeX-data-directory (straight--repos-dir "auctex")
+        TeX-lisp-direcoory TeX-data-directory
+
+  ;; ; Or custom-set-variables as follows.
+  ;; ; M-x describe-variable RET preview-TeX-style-dir RET
+  ;; '(preview-TeX-style-dir ,(concat ".:" (straight--repos-dir "auctex") "latex:"))
+  ;; preview-TeX-style-dir (concat ".:" (straight--repos-dir "auctex") "latex:")
+  ;;
+  ;; TeX-parse-self t ; parse on load
+  ;; TeX-auto-save t ; parse on save
+  ;; TeX-auto-untabify t ; automatically remove tabs before saving
+  ;;
+  ;; ; Type of TeX engine to use
+  ;; ;* 'default'
+  ;; ;* 'luatex'
+  ;; ;* 'omega'
+  ;; ;* 'xetex'
+  ;; TeX-source-correlate-mode t
+  ;; TeX-source-correlate-method 'synctex
+  ;; TeX-source-correlate-start-server t
+  ;;
+  ;; ; automatically insert braces after sub/superscript in math mode
+  ;; TeX-electric-sub-and-superscript t
+  ;; ; if non-nil, then query the user before saving each file with TeX-save-document
+  ;; TeX-save-query nil
+  ;; TeX-command-list '("XeLaTeX" "%'xelatex --synctex=1%(mode)%' %t"
+  ;;                     TeX-run-TeX nil t))
+  ;; TeX-view-program-list '(("eaf" 'eaf-pdf-synctex-forward-view))
+  ;; TeX-view-program-selection '((output-pdf "eaf"))
+  ;; ))
+  ))
+
+(eval-after-load "auctex"
+  '(add-to-list 'TeX-command-list
+                '("XeLaTeX" "xelatex -interaction=nonstopmode %s"
+                  TeX-run-command t t :help "Run xelatex") t ))
+
+;; (add-to-list 'Tex-command-list '("XeLaTeX" "%'xelatex --synctex=1%(mode)%' %t" TeX-run-TeX nil t))
+;;
+;; (eval-after-load "auctex"
+;;  '(add-to-list 'TeX-view-program-list '("eaf" eaf-pdf-synctex-forward-view) t))
+
+;; (eval-after-load "auctex"
+;;  '(add-to-list 'TeX-view-program-selection '(output-pdf "eaf") t))
 
 (provide 'init-auctex)
 ;;; init-auctex.el ends here
